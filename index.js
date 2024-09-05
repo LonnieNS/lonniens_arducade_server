d265dd693f938ff1bffca08566787155bc00e438
LonnieNS <lonn0558@mcdmstudent.dk>
2024-09-05 10:26:14 +0200

    hm

---
+++ index.js
@@ -0,0 +1,17 @@
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: `.env.local`, override: true })
+
+import server from './lib/server.js';
+
+const application = {}
+
+
+application.init = () => {
+
+    
+    server.run();
+
+}
+
+application.init();
